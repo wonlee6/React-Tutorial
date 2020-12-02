7ff53c7bae2629c07ac3f5bc9504a710a1e566f6
wonlee6 <wonlee6@gmail.com>
2020-12-02 21:47:49 +0900

    d

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
 
+const customer = {
+  'name' : '홍길동',
+  'birthday' : '960202',
+  'gender' : '남자',
+  'job' : '대학생'
+}
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo"></img>
-        <h2>Let's develop management system!.</h2>
-      </div>
+     <Customer 
+        name = {customer.name}
+        birthday = {customer.birthday}
+        gender = {customer.gender}
+        job = {customer.job}
+     />
     )
   }
 }
 
+// index.html 에서 root 에 뿌림
 export default App;

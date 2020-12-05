46fa45c25f5a9d0a7bdbce24dc51799042c527b2
wonlee6 <wonlee6@gmail.com>
2020-12-05 16:14:32 +0900

    rest api 구현하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,48 @@
-import React, { Component } from 'react';
-import Customer from './components/Customer'
-import './App.css';
-import Table from '@material-ui/core/Table';
-import Paper from '@material-ui/core/Paper';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-import { withStyles} from '@material-ui/core/styles';
+import React, { Component } from "react";
+import axios from "axios";
+import Customer from "./components/Customer";
+import "./App.css";
+import Table from "@material-ui/core/Table";
+import Paper from "@material-ui/core/Paper";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import { withStyles } from "@material-ui/core/styles";
 
-const styles = theme => ({
-  root : {
-    width : '100%',
-    marginTop : theme.spacing(3),
-    overflowX : "auto"
+
+const styles = (theme) => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+    overflowX: "auto",
   },
-  table : {
-    minWidth : 1080
-  }
-})
+  table: {
+    minWidth: 1080,
+  },
+});
 
-const customers = [
-  {
-  'id' : 1,
-  'image' : '123.jpg',
-  'name' : '홍길동',
-  'birthday' : '960202',
-  'gender' : '남자',
-  'job' : '대학생'
-},
-{
-  'id' : 2,
-  'image' : '1.jpg',
-  'name' : '이순신',
-  'birthday' : '121516',
-  'gender' : '남자',
-  'job' : '영웅'
-},
-{
-  'id' : 3,
-  'image' : '12.jpg',
-  'name' : '유이',
-  'birthday' : '921205',
-  'gender' : '여자',
-  'job' : '여배우'
-}
-]
 class App extends Component {
+  state = {
+    customers: ""
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await axios.get('./api/customers');
+    const body = await response.data
+    return body;
+  }
   render() {
     const { classes } = this.props;
     return (
-      <Paper className = {classes.root}>
-        <Table className = {classes.table}>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
           <TableHead>
             <TableRow>
               <TableCell>번호</TableCell>
@@ -63,25 +54,21 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-        {
-          customers.map(c => {
-            return (
-              <Customer
-              key = {c.id} // map 사용하기 위해 key 설정 필수 
-              id = {c.id}
-              image = {c.image}
-              name = {c.name}
-              birthday = {c.birthday}
-              gender = {c.gender}
-              job = {c.job}
-              />
-            );
-          })
-        }
+            {this.state.customers ? this.state.customers.map(c => {
+              return (<Customer
+                key={c.id} // map 사용하기 위해 key 설정 필수
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />);
+            }) : null}
           </TableBody>
         </Table>
-        </Paper>
-    )
+      </Paper>
+    );
   }
 }
 
--- server.js
+++ server.js
@@ -4,10 +4,35 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message : 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+  res.send([
+    {
+      'id': 1,
+      'image': '14.jpg',
+      'name': '홍길동',
+      'birthday': '960202',
+      'gender': '남자',
+      'job': '대학생',
+    },
+    {
+      'id': 2,
+      'image': "1.jpg",
+      'name': "이순신",
+      'birthday': "121516",
+      'gender': "남자",
+      'job': "영웅",
+    },
+    {
+      'id': 3,
+      'image': "12.jpg",
+      'name': "유이",
+      'birthday': "921205",
+      'gender': "여자",
+      'job': "여배우",
+    }
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

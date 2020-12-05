ee735ec5b858eacbbffe13eb76507c614e95323e
wonlee6 <wonlee6@gmail.com>
2020-12-05 19:31:08 +0900

    insert

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
-const { json } = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -22,6 +21,9 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
+const multer = require('multer');
+const upload = multer({ dest: './upload' });
+
 app.get('/api/customers', (req, res) => {
   connection.query(
     "SELECT * FROM CUSTOMER",
@@ -30,5 +32,23 @@ app.get('/api/customers', (req, res) => {
     }
   )
 });
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+  let sql = 'INSERT INTO CUSTOMER VALUES(NULL,?,?,?,?,?)';
+  let image = 'http://localhost:5000/image/' + req.file.filename;
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+  let params = [image, name, birthday, gender, job];
+  connection.query(sql, params,
+    (err, rows, fields) => {
+      res.send(rows);
+      console.log(err)
+      console.log(rows)
+    })
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
